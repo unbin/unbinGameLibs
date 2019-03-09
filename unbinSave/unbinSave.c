a5303feae8f55676562dfc97c8eff82188e77ed5
unbin <unbin1234@gmail.com>
2019-03-09 12:46:05 -0500

    Cleaned up some code in unbinSave.c

--- unbinSave/unbinSave.c
+++ unbinSave/unbinSave.c
@@ -5,31 +5,29 @@
 #define CHARREADSIZE 1000
 
 bool unbinSave_Save(const char *path, const char *saveData) {
-  bool success = true;
   FILE *fp = fopen(path, "w");
-  if (!fp) {
-    fprintf(stderr, "unbinSave Error: Could not open file %s for writing\n", path);
-    success = false;
+  if (fp) {
+    if (fprintf(fp, "%s\n", saveData) < 0) {
+      fprintf(stderr, "unbinSave Error: Could not write save data to %s\n", path);
+      return false;
+    }
+    fclose(fp);
+    return true;
   }
-  else if (fprintf(fp, "%s\n", saveData) < 0) {
-    fprintf(stderr, "unbinSave Error: Could not write save data to %s\n", path);
-    success = false;
-  }
-  if (fp) fclose(fp);
-  return success;
+  fprintf(stderr, "unbinSave Error: Could not open file %s for writing\n", path);
+  return false;
 }
 
 bool unbinSave_Load(const char *path, char *saveData) {
-  bool success = true;
   FILE *fp = fopen(path, "r");
-  if (!fp) {
-    fprintf(stderr, "unbinSave Error: Could not open file %s for reading\n", path);
-    success = false;
+  if (fp) {
+    char charBuffer[CHARREADSIZE];
+    while (fscanf(fp, "%s", charBuffer) != EOF) {
+      strcat(saveData, charBuffer);
+    }
+    fclose(fp);
+    return true;
   }
-  char charBuffer[CHARREADSIZE];
-  while (fscanf(fp, "%s", charBuffer) != EOF) {
-    strcat(saveData, charBuffer);
-  }
-  if (fp) fclose(fp);
-  return success;
+  fprintf(stderr, "unbinSave Error: Could not open file %s for reading\n", path);
+  return false;
 }
